7a62f1c2cbeb8fc2eb1c89f014d9c879b6c1bf6d
alistonroier <aliston.p@icloud.com>
2021-06-10 19:55:01 -0300

    Sexto commit - Atualizado Estratégia do cooperante (acrescentado na class personagemCooperante

--- src/jogoevo/JogoEvo.java
+++ src/jogoevo/JogoEvo.java
@@ -11,8 +11,10 @@ public class JogoEvo {
     public static void main(String[] args) {
         
         
-        Scanner entrada = new Scanner(System.in);     
+        Scanner entrada = new Scanner(System.in);  
+        
         cenario inijogo = new cenario();
+        personagemCooperante personagemCoo = new personagemCooperante();
         
         System.out.println(inijogo.introducao()); //tex de introdução do game
 
@@ -20,40 +22,7 @@ public class JogoEvo {
         //adversarioEscolhido = inijogo.selecionaPersonagem(); //caso precise escolher o personagem (class cenario)
 
         // Primeiro adversário o cooperante
-        int i = 0;
-        int moedasUsuario = 0;
-        int moedasAdversario = 0;
-        int totalMoedas = 0;
-
-        while(i<inijogo.jogadasAleatorias()){//while com quantidade de jogas gerada
-        System.out.println("\nOPONENTE: 1 DE 2");
-        System.out.println("SUA PONTUAÇÃO TOTAL: "+totalMoedas);
-        System.out.println("1 - ENGANAÇÃO \n2 - COLABORAR");
-        int opcaoMoeda = entrada.nextInt();
-            if(opcaoMoeda < 1 ||opcaoMoeda > 2){
-                System.out.println("Opção incorreta\n");//retorna pra colocar 1 ou 2
-                
-                }else if(opcaoMoeda == 1){
-                    //se o jogador enganar acontece isso                    
-                    moedasUsuario = moedasUsuario + 3;
-                    moedasAdversario--;
-                    System.out.println("\n+ 3 MOEDAS para o jogador = "+moedasUsuario+" MOEDAS");
-                    System.out.println("Outro jogador COOPEROU");
-                    System.out.println("- 1 MOEDA para o adversário = "+moedasAdversario+" MOEDAS");
-                    i++;
-                    totalMoedas = moedasUsuario;
-                    
-                }else{
-                    //se o jogador enganar acontece isso
-                    moedasUsuario = moedasUsuario + 2;
-                    moedasAdversario = moedasAdversario + 2;
-                    System.out.println("+ 2 MOEDAS para o jogador = "+moedasUsuario+" MOEDAS");
-                    System.out.println("Outro jogador COOPEROU");
-                    System.out.println("+ 2 MOEDAS para o adversário = "+moedasAdversario+" MOEDAS");
-                    i++;
-                    totalMoedas = moedasUsuario;
-
-                }
-        }      
+        personagemCoo.estrategiaCooperante();
+             
     }
 }
--- src/jogoevo/personagemCooperante.java
+++ src/jogoevo/personagemCooperante.java
@@ -1,19 +1,48 @@
 
 package jogoevo;
 
+import java.util.Scanner;
 
 public class personagemCooperante {
- 
-        
-        String nome = "Cooperante";
-        boolean insereMoeda = true;
-        int qtdMoedaInserida = 1;
-        int pontosCooperante = 0;
+    cenario inijogo = new cenario();
+    Scanner entrada = new Scanner(System.in);     
         
     public int estrategiaCooperante(){
-    int i = 0;
-    return i;
-    //acrescentar aqui o que está no JogoEvo.java
+        int i = 0;
+        int moedasUsuario = 0;
+        int moedasAdversario = 0;
+        int totalMoedas = 0;
+
+        while(i<inijogo.jogadasAleatorias()){//while com quantidade de jogas gerada
+        System.out.println("\nOPONENTE: 1 DE 2");
+        System.out.println("SUA PONTUAÇÃO TOTAL: "+totalMoedas);
+        System.out.println("1 - ENGANAÇÃO \n2 - COLABORAR");
+        int opcaoMoeda = entrada.nextInt();
+            if(opcaoMoeda < 1 ||opcaoMoeda > 2){
+                System.out.println("Opção incorreta\n");//retorna pra colocar 1 ou 2
+                
+                }else if(opcaoMoeda == 1){
+                    //se o jogador enganar acontece isso                    
+                    moedasUsuario = moedasUsuario + 3;
+                    moedasAdversario--;
+                    System.out.println("\n+ 3 MOEDAS para o jogador = "+moedasUsuario+" MOEDAS");
+                    System.out.println("Outro jogador COOPEROU");
+                    System.out.println("- 1 MOEDA para o adversário = "+moedasAdversario+" MOEDAS");
+                    i++;
+                    totalMoedas = moedasUsuario;
+                    
+                }else{
+                    //se o jogador enganar acontece isso
+                    moedasUsuario = moedasUsuario + 2;
+                    moedasAdversario = moedasAdversario + 2;
+                    System.out.println("+ 2 MOEDAS para o jogador = "+moedasUsuario+" MOEDAS");
+                    System.out.println("Outro jogador COOPEROU");
+                    System.out.println("+ 2 MOEDAS para o adversário = "+moedasAdversario+" MOEDAS");
+                    i++;
+                    totalMoedas = moedasUsuario;
+                }
+        } 
+    return totalMoedas;
     }
     
 }
